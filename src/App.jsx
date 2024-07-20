43268f86a3179b63dd697a062074b056df4fb56a
Midhun Chandran <midhun@micro.company>
2024-07-20 13:43:31 +0530

    feat: initial commit, setup project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MemoryGame from './Pages/MemoryGame';
+
+function App() {
+  return (
+    <div className="App">
+      <MemoryGame />
+    </div>
+  );
+}
+
+export default App;
