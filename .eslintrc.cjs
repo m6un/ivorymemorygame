f4720d53288727f2cb6513b30f726815126232a3
Midhun Chandran <midhun@micro.company>
2024-07-20 13:44:01 +0530

    feat: add all the config setup including tailwind and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,5 @@ module.exports = {
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    'prettier/prettier': 'error',
   },
 };
